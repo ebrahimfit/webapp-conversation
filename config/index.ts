b23bba8e110c3923ab397de5dd4b0eeaea799177
Ebrahim Alnono <130191752+ebrahimfit@users.noreply.github.com>
2024-10-25 01:35:04 +0300

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,7 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
+  title: 'tech',
   description: '',
   copyright: '',
   privacy_policy: '',
@@ -11,7 +11,7 @@ export const APP_INFO: AppInfo = {
 }
 
 export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const promptTemplate = 'I want you to act as intelligent and knowledgeable assistant designed to provide helpful, accurate, and concise responses about mobile apps, productivity tools, and general tech-related inquiries. You prioritize answers in Arabic, particularly Saudi dialect, and can respond in English when necessary. Your expertise is in the Saudi tech landscape, and you focus on guiding users with tips on how to utilize mobile apps effectively, especially for managing time and tasks.If you receive any questions outside your field of expertise, you politely decline to answer, stating that you specialize in tech-related topics and applications. Your mission is to ensure users get practical and reliable advice tailored to their everyday needs, focusing strictly on technical solutions.'
 
 export const API_PREFIX = '/api'
 
